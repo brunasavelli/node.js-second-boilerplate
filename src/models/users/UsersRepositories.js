5e5a343bfe4979c4e12a53690c8410a5d23629a5
Bruna Savelli <bnsavelli@gmail.com>
2024-10-31 15:23:40 -0300

    feat: create get all users method

---
+++ src/models/users/UsersRepositories.js
@@ -0,0 +1,11 @@
+class UsersRepositories{
+    constructor(){
+     this.users = [];   
+    }
+
+    getAllUsers(){
+        return this.users;
+    }
+}
+
+export default UsersRepositories;
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
+import UsersRepositories from "../models/users/UsersRepositories.js";
 
 const usuariosRoutes = Router();
-
-let usuarios = [];
+const usersList = new UsersRepositories();
 
 usuariosRoutes.get("/", (req, res) => {
+  const usuarios = usersList.getAllUsers();
   return res.status(200).json({
     message: 
     usuarios.length == 0 
     ? "Não há usuários cadastrados"
     : `Total de usuários: ${usuarios.length}`,
-    usuarios,
   });
 });
 
